c66577497520f7f1590df04e22d642331d023804
tien06051992 <tien06051992@gmail.com>
2013-09-16 11:57:43 +0700

    1) HTML code finished 2) style <header> finished

--- __html/js/main.js
+++ __html/js/main.js
@@ -1,1 +1,8 @@
-
+jQuery(document).ready(function($) {  
+    $('#myTab a[href="#random"]').tab('show');
+    $('#myTab a').click(function (e) {
+    e.preventDefault();
+     $(this).tab('show');
+    });
+    
+});
