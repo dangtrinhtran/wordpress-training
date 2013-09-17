59577db67aa10830bc3abc081d76ecc346f5a072
tien06051992 <tien06051992@gmail.com>
2013-09-17 15:55:55 +0700

    completed HTML Bootstrap

--- __html/js/main.js
+++ __html/js/main.js
@@ -1,13 +1,8 @@
 jQuery(document).ready(function($) {  
     $('#myTab a[href="#random"]').tab('show');
-    $('#myTab a').click(function (e) {
-    e.preventDefault();
-     $(this).tab('show');
+    $(window).load(function () {
+    // start the slideshow
+    $('.slideshow').blinds();
     });
-    $('#s4').before('<div id="nav_slide">').cycle({ 
-    fx:     'turnDown', 
-    speed:  'fast', 
-    timeout: 0, 
-    pager:  '#nav' 
-});
+    document.write("SDSDFsdfsdfsdf");
 });
