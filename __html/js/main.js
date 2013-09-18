74044ffb9b2fe2b9fef14cc80fa1ba1aa7f08e28
tien06051992 <tien06051992@gmail.com>
2013-09-18 10:42:18 +0700

    1) index main.js

--- __html/js/main.js
+++ __html/js/main.js
@@ -1,8 +1,7 @@
 jQuery(document).ready(function($) {  
     $('#myTab a[href="#random"]').tab('show');
-    $(window).load(function () {
+//    $(window).load(function () {
     // start the slideshow
     $('.slideshow').blinds();
-    });
-    document.write("SDSDFsdfsdfsdf");
+   // });
 });
